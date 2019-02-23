ff1fcffd89f43a29d0472b347398f8754bb14e2b
Brad Niedt <bradleyniedt@gmail.com>
2019-02-23 14:49:39 -0700

    Adds namespaced route for backgrounds endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: "forecast#index"
-    end 
+      get '/backgrounds', to: "background#index"
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
