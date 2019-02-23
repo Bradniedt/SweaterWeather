4eb5205d63f99c507b63c88788f342cf6f369ae6
Brad Niedt <bradleyniedt@gmail.com>
2019-02-23 11:56:51 -0700

    Adds change to forecast controller index method to reflect rearrangement of service workflow.

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     coords = GeocodeService.new.get_coords(params[:location])
-    forecast = ForecastService.new.get_forecast("#{coords["lat"]},#{coords["lng"]}")
+    forecast = Forecast.get_weather("#{coords["lat"]},#{coords["lng"]}")
     binding.pry
     render json: ForecastSerializer.new(forecast)
   end
