873e053ac9f515faf5e6b4f2d0e273138738fca8
Brad Niedt <bradleyniedt@gmail.com>
2019-02-23 15:10:35 -0700

    Adds attribute tests for forecast.

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -2,16 +2,23 @@ require 'rails_helper'
 
 describe Forecast, type: :model do
   before :each do
-    data = {
+    @data = {
             "timezone" => 'a timezone',
             "currently" => 'a currently',
             "daily" => 'a daily',
             "hourly" => 'a hourly',
             }
-    @forecast = Forecast.new(data)
+    @forecast = Forecast.new(@data)
   end
   it 'exists' do
     expect(@forecast).to be_an_instance_of(Forecast)
+  end
+  it 'has_attributes' do
+    expect(@forecast.id).to eq(1)
+    expect(@forecast.timezone).to eq(@data["timezone"])
+    expect(@forecast.currently).to eq(@data["currently"])
+    expect(@forecast.daily).to eq(@data["daily"])
+    expect(@forecast.hourly).to eq(@data["hourly"])
   end
   describe 'class methods' do
     it 'get_weather' do
