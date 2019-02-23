0f249cc71cc3065ea38d36ded8e7b42363649b81
Brad Niedt <bradleyniedt@gmail.com>
2019-02-23 13:13:59 -0700

    Adds change to pass feature test.

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::ForecastController < ApplicationController
   def index
     coords = GeocodeService.new.get_coords(params[:location])
     forecast = Forecast.get_weather("#{coords["lat"]},#{coords["lng"]}")
-    binding.pry
     render json: ForecastSerializer.new(forecast)
   end
 end
