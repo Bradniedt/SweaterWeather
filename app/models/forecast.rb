1a6c937eef185d8a1ea536f9eed98c4f02944c8a
Brad Niedt <bradleyniedt@gmail.com>
2019-02-23 12:54:20 -0700

    Adds cassettes to gitignore, removed photos attribute from forecast poro, adds change to forecast serializer to remove photo.

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,13 +1,11 @@
 class Forecast
-  attr_reader :id, :weather_report, :currently, :daily, :hourly, :photo
+  attr_reader :id, :weather_report, :currently, :daily, :hourly
   def initialize(data)
     @id = 1
     @weather_report = data
     @current = data["currently"]
     @daily = data["daily"]
     @hourly = data["hourly"]
-    pic = PhotoService.new.get_photo(data["latitude"], data["longitude"])
-    @photo = pic
   end
 
   def self.get_weather(lat, lon)
--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,4 +1,4 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :weather_report, :currently, :daily, :hourly, :photo
+  attributes :id, :weather_report, :currently, :daily, :hourly
 end
