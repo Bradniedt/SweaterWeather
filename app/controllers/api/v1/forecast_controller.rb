cfb39534f8d7d4cc7b3d7ed1b96a98672d3d4d71
Brad Niedt <bradleyniedt@gmail.com>
2019-02-23 11:42:10 -0700

    Adds new lines to forecast index method to dream drive.

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     coords = GeocodeService.new.get_coords(params[:location])
+    forecast = ForecastService.new.get_forecast("#{coords["lat"]},#{coords["lng"]}")
     binding.pry
-    forecast = ForecastService.new.get_forecast(coords["lat"], coords["lng"])
     render json: ForecastSerializer.new(forecast)
   end
 end
