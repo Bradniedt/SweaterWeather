00e1f2388423effc274f90989bed21b7c5e18e31
Brad Niedt <bradleyniedt@gmail.com>
2019-02-22 18:42:48 -0700

    Adds first tests for geocode service.

---
+++ spec/services/geocode_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Geocode service' do
+  before :each do
+    @service = GeocodeService.new
+  end
+  it 'exists' do
+    expect(@service).to be_an_instance_of(GeocodeService)
+  end
+  it 'returns coordinates when given a city and state' do
+    data = @service.get_coords("Denver, CO")
+
+    expect(data).to have_key("lat")
+    expect(data).to have_key("lng")
+  end
+end
