8e0e0bf246f931a44e37eff24437311d14b5e865
Brad Niedt <bradleyniedt@gmail.com>
2019-02-23 13:01:01 -0700

    Adds change to forecast model to reflect needs of view.

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,8 +1,8 @@
 class Forecast
-  attr_reader :id, :weather_report, :currently, :daily, :hourly
+  attr_reader :id, :timezone, :currently, :daily, :hourly
   def initialize(data)
     @id = 1
-    @weather_report = data
+    @timezone = data["timezone"]
     @current = data["currently"]
     @daily = data["daily"]
     @hourly = data["hourly"]
