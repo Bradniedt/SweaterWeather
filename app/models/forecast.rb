27e71204ffc22aa5096f7daf3648dd05148b0391
Brad Niedt <bradleyniedt@gmail.com>
2019-02-23 11:48:12 -0700

    Adds all attributes to forecast model.

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,10 +1,11 @@
 class Forecast
-  attr_reader :id, :weather_report, :currently, :daily, :hourly
+  attr_reader :id, :weather_report, :currently, :daily, :hourly, :photo
   def initialize(data)
     @id = 1
     @weather_report = data
     @current = data["currently"]
     @daily = data["daily"]
     @hourly = data["hourly"]
+    @photo = PhotoService.new.get_photo(data["latitude"], data["longitude"])
   end
 end
