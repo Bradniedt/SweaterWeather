1c95648af4d895df9f40a5339479bd0fdc9f3d78
Brad Niedt <bradleyniedt@gmail.com>
2019-02-23 11:16:06 -0700

    Adds fast json api gem and forecast controller index method."

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-
-  end 
+    coords = GeocodeService.new.get_coords(params[:location])
+    binding.pry
+    forecast = ForecastService.new.get_forecast(coords["lat"], coords["lng"])
+    render json: ForecastSerializer.new(forecast)
+  end
 end
