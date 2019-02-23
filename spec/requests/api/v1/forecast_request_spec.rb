8c3582b8645ace9cf28d42fb25dcd4f0fdaa27b1
Brad Niedt <bradleyniedt@gmail.com>
2019-02-23 11:43:14 -0700

    Removes lines from forecast test to match json return.

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -2,27 +2,16 @@ require 'rails_helper'
 
 describe 'Forecast API' do
   it 'should return JSON data for a forecast' do
-    get  '/api/v1/forecast?location=denver,co'
+    VCR.use_cassette('forecast') do
+      get  '/api/v1/forecast?location=denver,co'
 
-    data = JSON.parse(response.body)
-    expect(response).to be_successful
-    expect(data).to have_key("city")
-    expect(data).to have_key("state")
-    expect(data).to have_key("country")
-    expect(data).to have_key("latitude")
-    expect(data).to have_key("longitude")
-    expect(data).to have_key("current_weather_type")
-    expect(data).to have_key("current_temp")
-    expect(data).to have_key("high_temp")
-    expect(data).to have_key("low_temp")
-    expect(data).to have_key("feels_like")
-    expect(data).to have_key("humidity")
-    expect(data).to have_key("visibility")
-    expect(data).to have_key("uv_index")
-    expect(data).to have_key("today_sentence")
-    expect(data).to have_key("tonight_sentence")
-    expect(data).to have_key("daily")
-    expect(data).to have_key("hourly")
-    expect(data).to have_key("photo")
+      data = JSON.parse(response.body)
+      expect(response).to be_successful
+      expect(data).to have_key("latitude")
+      expect(data).to have_key("longitude")
+      expect(data).to have_key("timezone")
+      expect(data).to have_key("currently")
+      expect(data).to have_key("hourly")
+    end
   end
 end
