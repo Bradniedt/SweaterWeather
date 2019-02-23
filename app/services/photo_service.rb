03f35e26495e755269dd5a494851ae7e848f1466
Brad Niedt <bradleyniedt@gmail.com>
2019-02-23 12:51:39 -0700

    Adds changes to photo service to pass photo service test.

--- app/services/photo_service.rb
+++ app/services/photo_service.rb
@@ -5,14 +5,25 @@ class PhotoService
     end
   end
 
-  def get_photo(coords)
+  def get_data(lat, lon)
     response = conn.get("services/rest/") do |request|
       request.params['method'] = "flickr.photos.getRecent"
       request.params['api_key'] = ENV['FLICKR_KEY']
       request.params['format'] = "json"
-      request.params['lat'] = coords["lat"]
-      request.params['lon'] = coords["lng"]
+      request.params['lat'] = lat
+      request.params['lon'] = lon
+      request.params['nojsoncallback'] = "true"
+      request.params['safe_search'] = "1"
     end
     results = JSON.parse(response.body)
+  end
+
+  def build_url(data)
+    "https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg"
+  end
+
+  def get_photo(location)
+    coords = GeocodeService.new.get_coords(location)
+    get_data(coords["lat"], coords["lng"])
   end
 end
