c7ba1c8538ac560ef33c347c448a1bedafc541d2
Brad Niedt <bradleyniedt@gmail.com>
2019-02-24 12:51:28 -0700

    Adds creation of api_key, creating user, and a final render command. Will start testing the login.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    key = SecureRandom.base64
+    user = User.create!(email: params["email"], password: params["password"], api_key: key)
     binding.pry
+    render json: ApiSerializer.new(user)
   end
 end
