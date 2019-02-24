f55f7d7542ef2fe6c068373ff31c4babe58a6876
Brad Niedt <bradleyniedt@gmail.com>
2019-02-24 12:53:08 -0700

    Removes pry to confirm test passes.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::UsersController < ApplicationController
   def create
     key = SecureRandom.base64
     user = User.create!(email: params["email"], password: params["password"], api_key: key)
-    binding.pry
     render json: ApiSerializer.new(user)
   end
 end
