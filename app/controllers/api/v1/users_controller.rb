23a081ee9165fc1b9aff26c7af2e76dcf74704c9
Brad Niedt <bradleyniedt@gmail.com>
2019-02-24 13:22:18 -0700

    Adds user params to make registration more secure.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   def create
     key = SecureRandom.base64
-    user = User.create!(email: params["email"], password: params["password"], api_key: key)
+    user = User.create!(email: user_params["email"], password: user_params["password"], api_key: key)
     render json: ApiSerializer.new(user)
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:email, :password)
   end
 end
