64ebc9702f600fa632e35f90ef449072ac0e70f1
Brad Niedt <bradleyniedt@gmail.com>
2019-02-24 12:36:05 -0700

    Adds syntax for sending params through post in the login test.

--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 describe '(Login Endpoint) as a new user' do
   describe 'as a new user, when I send a post request to /api/v1/users with an email, password, and password confirmation' do
+    before { post '/api/v1/users', params: { "email" => "bob@email.com", "password" => "pass", "password_confirmation" => "pass"} }
     it 'should return my api key in a JSON response' do
-      params = { :user => { "email" => "bob@email.com", "password" => "pass", "password_confirmation" => "pass"}}
-      post  '/api/v1/users', params.to_json, {'ACCEPT' => "application/json", 'CONTENT_TYPE' => 'application/json'}
 
       data = JSON.parse(response.body)["data"]["attributes"]
       expect(response).to be_successful
