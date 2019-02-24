37a458aa20b1a8be108ae3bb3f01d42260f365fc
Brad Niedt <bradleyniedt@gmail.com>
2019-02-24 13:21:40 -0700

    Adds new expectation to login test.

--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 describe '(Login Endpoint) as a new user' do
   describe 'as a new user, when I send a post request to /api/v1/users with an email, password, and password confirmation' do
-    before { post '/api/v1/users', params: { "email" => "bob@email.com", "password" => "pass", "password_confirmation" => "pass"} }
+    before { post '/api/v1/users', params: { :user => { "email" => "bob@email.com", "password" => "pass", "password_confirmation" => "pass"} }}
     it 'should return my api key in a JSON response' do
 
       data = JSON.parse(response.body)["data"]["attributes"]
       expect(response).to be_successful
       expect(data).to have_key("api_key")
+      expect(data["api_key"].length).to eq(24)
     end
   end
 end
