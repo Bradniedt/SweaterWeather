7d8eb4ec6c6d386d90ac09624d912a8970f5afb7
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 10:18:35 -0700

    Changes split and renames method.

--- spec/services/gif_service_spec.rb
+++ spec/services/gif_service_spec.rb
@@ -9,9 +9,9 @@ describe 'Gif service' do
   end
   it 'returns a gif url when given search terms' do
     VCR.use_cassette("gif_service") do
-      data = @service.get_forecast("Partly cloudy")
+      data = @service.get_gif("Partly cloudy")
 
-      expect(data.slice(0, 1, 2, 3)).to eq("http")
+      expect(data.slice(0, 3)).to eq("http")
     end
   end
 end
