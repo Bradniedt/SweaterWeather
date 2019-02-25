9fe9f863d0567d9e50ce439c2c70328724211888
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 11:11:32 -0700

    Changes the gif controller to call the gif serializer that has a gif presenter passed to it.

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GifsController < ApplicationController
   def index
     forecasts = Giforecast.get_weather_gifs(params["location"])
-    render json: GiforecastSerializer.new(forecasts)
+    render json: GifSerializer.new(GifPresenter.new(forecasts))
   end
 end
