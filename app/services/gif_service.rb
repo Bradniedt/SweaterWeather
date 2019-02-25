a4c63e143c6f920b154b6da7109efd16e2c96ff3
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 10:11:34 -0700

    Adds mothods to get gif service up and running

--- app/services/gif_service.rb
+++ app/services/gif_service.rb
@@ -5,11 +5,16 @@ class GifService
     end
   end
 
-  def get_gif(terms)
+  def get_url(url, search_terms)
     response = conn.get("v1/gifs/search") do |request|
       request.params["api_key"] = ENV['GIPHY_KEY']
-      request.params["q"] = terms
-    end 
-    results = JSON.parse(response.body)
+      request.params["q"] = search_terms
+      request.params["limit"] = 5
+    end
+  end
+
+  def get_gif(terms)
+    response = get_url("v1/gifs/search", terms)
+    result = JSON.parse(response.body)["data"].shuffle.first["url"]
   end
 end
