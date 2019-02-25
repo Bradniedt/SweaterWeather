9ce53badbc7f3fe05590252f26828e1e46a6060a
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 10:17:27 -0700

    Adds change to gif service test.

---
+++ spec/services/gif_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Gif service' do
+  before :each do
+    @service = GifService.new
+  end
+  it 'exists' do
+    expect(@service).to be_an_instance_of(GifService)
+  end
+  it 'returns a gif url when given search terms' do
+    VCR.use_cassette("gif_service") do
+      data = @service.get_forecast("Partly cloudy")
+
+      expect(data.slice(0, 1, 2, 3)).to eq("http")
+    end
+  end
+end
