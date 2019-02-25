7cc34584d717282687ff18e65dbce05a7dc9a001
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 11:17:54 -0700

    Cleans up users controller.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,10 +4,4 @@ class Api::V1::UsersController < ApplicationController
     user = User.create!(email: params["email"], password: params["password"], api_key: key)
     render json: ApiSerializer.new(user)
   end
-
-  private
-
-  def user_params
-      params.require(:user).permit(:email, :password)
-  end
 end
