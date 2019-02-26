f0cc9320e39f6d742114ea0c089d0a19f6cba2b1
Brad Niedt <bradleyniedt@gmail.com>
2019-02-26 13:24:13 -0700

    Adds location table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_030053) do
+ActiveRecord::Schema.define(version: 2019_02_26_201812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2019_02_26_030053) do
     t.string "location"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "latitude"
+    t.string "longitude"
   end
 
   create_table "users", force: :cascade do |t|
