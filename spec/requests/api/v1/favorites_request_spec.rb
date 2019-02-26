6ece131f901e04002ced2bf3e0cc0f06931cd5eb
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 20:07:33 -0700

    Adds sad path test for creating favorite.

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe '(Favorites Endpoint) As a user' do
   describe 'when I send a POST request with a location and my api key to /api/v1/favorites' do
     before do
-      @key = SecureRandom.base64
-      @user = User.create!(email: "bob@email.com", password: "pass", api_key: @key)
+      @key1 = SecureRandom.base64
+      @user1 = User.create!(email: "bob@email.com", password: "pass", api_key: @key)
       post '/api/v1/favorites', params: { "api_key" => "#{@key}", "location" => "Denver, CO" }
     end
     it 'should add that location to my favorites' do
@@ -13,6 +13,18 @@ describe '(Favorites Endpoint) As a user' do
       expect(response).to be_successful
       expect(favorite.user_id).to eq(user.id)
       expect(favorite.location).to eq("Denver, CO")
+    end
+    before do
+      @key2 = SecureRandom.base64
+      @bad_key = SecureRandom.base64
+      @user2 = User.create!(email: "bob@email.com", password: "pass", api_key: @key)
+      post '/api/v1/favorites', params: { "api_key" => "#{@bad_key}", "location" => "Denver, CO" }
+    end
+    it 'should not add that location to my favorites if my api key is wrong' do
+      user = User.find(@user.id)
+
+      expect(response).to_not be_successful
+      expect(user.favorites).to eq([])
     end
   end
 end
