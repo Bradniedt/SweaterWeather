78a5dcccb956a22b0a7bab4a72d600f7b98b8ea0
Brad Niedt <bradleyniedt@gmail.com>
2019-02-26 15:17:17 -0700

    Adds vcr to favorite presenter tests.

--- spec/presenters/favorite_presenter_spec.rb
+++ spec/presenters/favorite_presenter_spec.rb
@@ -12,26 +12,32 @@ describe 'Favorite Presenter' do
     @user2.favorites.create(location: "Dillon, CO")
   end
   it 'exists' do
-    faves = @user.favorites
-    presenter = FavoritePresenter.new(faves)
-    expect(presenter).to be_an_instance_of(FavoritePresenter)
+    VCR.use_cassette('fave_presenter_1') do
+      faves = @user.favorites
+      presenter = FavoritePresenter.new(faves)
+      expect(presenter).to be_an_instance_of(FavoritePresenter)
+    end
   end
   it 'has_attributes' do
-    faves = @user.favorites
-    presenter = FavoritePresenter.new(faves)
-    expect(presenter.id).to eq(1)
-    expect(presenter.favorites).to be_an_instance_of Array
-    expect(presenter.favorites.first).to have_key("location")
-    expect(presenter.favorites.first).to have_key("current_weather")
-    expect(presenter.favorites.first["location"]).to eq("Denver, CO")
+    VCR.use_cassette('fave_presenter_2') do
+      faves = @user.favorites
+      presenter = FavoritePresenter.new(faves)
+      expect(presenter.id).to eq(1)
+      expect(presenter.favorites).to be_an_instance_of Array
+      expect(presenter.favorites.first).to have_key("location")
+      expect(presenter.favorites.first).to have_key("current_weather")
+      expect(presenter.favorites.first["location"]).to eq("Denver, CO")
+    end
   end
   it 'delete_favorite' do
-    fave = @user2.favorites.first
-    presenter = FavoritePresenter.new(fave, "delete")
-    all_faves = User.find(@user2.id).favorites
-    expect(presenter.id).to eq(1)
-    expect(presenter.favorites.first).to have_key("location")
-    expect(presenter.favorites.first["location"]).to eq("Denver, CO")
-    expect(all_faves.length).to eq(1)
+    VCR.use_cassette('fave_presenter_3') do
+      fave = @user2.favorites.first
+      presenter = FavoritePresenter.new(fave, "delete")
+      all_faves = User.find(@user2.id).favorites
+      expect(presenter.id).to eq(1)
+      expect(presenter.favorites.first).to have_key("location")
+      expect(presenter.favorites.first["location"]).to eq("Denver, CO")
+      expect(all_faves.length).to eq(1)
+    end
   end
 end
