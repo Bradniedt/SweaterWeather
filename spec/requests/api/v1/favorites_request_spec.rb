e7921d65597f4518a08ccee9f45c07a478d2a86b
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 19:07:27 -0700

    Adds new dream driven test for the favorites endpoint.

---
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe '(Favorites Endpoint) As a user' do
+  describe 'when I send a POST request with a location and my api key to /api/v1/favorites' do
+    before do
+      @key = SecureRandom.base64
+      @user = User.create!(email: "bob@email.com", password: "pass", api_key: @key)
+      post '/api/v1/favorites', params: { "api_key" => "#{@key}", "location" => "Denver, CO" }
+    end
+    it 'should add that location to my favorites' do
+      VCR.use_cassette('favorites_request') do
+        user = User.find(@user.id)
+        favorite = user.favorites.first
+        expect(response).to be_successful
+        expect(favorite.user_id).to eq(user.id)
+        expect(favorite.location).to eq("Denver, CO")
+        expect(favorite.current_weather).to have_key("time")
+        expect(favorite.current_weather).to have_key("summary")
+        expect(favorite.current_weather).to have_key("temperature")
+      end
+    end
+  end
+end
