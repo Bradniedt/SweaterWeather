bc00f0373f41b7069671425bc977e3088c37e303
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 20:21:53 -0700

    Replaced test name.

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe User, type: :model do
+describe Favorite, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:location)}
     it { should belong_to(:user)}
