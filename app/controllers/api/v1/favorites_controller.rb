12bf5510559e8df36a9e6e3f7a9089fdb2e2cb78
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 20:05:30 -0700

    Changed user favorite creation.

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::FavoritesController < ApplicationController
     binding.pry
     user = User.find_by(api_key: params["api_key"])
     if user
-      user.create_favorite(params["location"])
+      user.favorites.create!(params["location"])
       render json: {}, status: 201
     else
       render json: {}, status: 401
