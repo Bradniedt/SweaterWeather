05ac89b139028a584e07ad94547ef6e5b5d59d59
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 20:08:48 -0700

    Fixes small errors in user create.

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -4,7 +4,7 @@ describe '(Favorites Endpoint) As a user' do
   describe 'when I send a POST request with a location and my api key to /api/v1/favorites' do
     before do
       @key1 = SecureRandom.base64
-      @user1 = User.create!(email: "bob@email.com", password: "pass", api_key: @key)
+      @user1 = User.create!(email: "bob@email.com", password: "pass", api_key: @key1)
       post '/api/v1/favorites', params: { "api_key" => "#{@key}", "location" => "Denver, CO" }
     end
     it 'should add that location to my favorites' do
@@ -17,7 +17,7 @@ describe '(Favorites Endpoint) As a user' do
     before do
       @key2 = SecureRandom.base64
       @bad_key = SecureRandom.base64
-      @user2 = User.create!(email: "bob@email.com", password: "pass", api_key: @key)
+      @user2 = User.create!(email: "george@email.com", password: "pass", api_key: @key2)
       post '/api/v1/favorites', params: { "api_key" => "#{@bad_key}", "location" => "Denver, CO" }
     end
     it 'should not add that location to my favorites if my api key is wrong' do
