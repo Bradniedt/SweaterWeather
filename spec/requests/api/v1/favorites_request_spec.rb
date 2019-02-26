24c7c20fc7c0a2c2d9966453f48aa94b6d75c73a
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 20:09:14 -0700

    Fixes small errors in user create.

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -8,8 +8,9 @@ describe '(Favorites Endpoint) As a user' do
       post '/api/v1/favorites', params: { "api_key" => "#{@key}", "location" => "Denver, CO" }
     end
     it 'should add that location to my favorites' do
-      user = User.find(@user.id)
+      user = User.find(@user1.id)
       favorite = user.favorites.first
+      
       expect(response).to be_successful
       expect(favorite.user_id).to eq(user.id)
       expect(favorite.location).to eq("Denver, CO")
@@ -21,7 +22,7 @@ describe '(Favorites Endpoint) As a user' do
       post '/api/v1/favorites', params: { "api_key" => "#{@bad_key}", "location" => "Denver, CO" }
     end
     it 'should not add that location to my favorites if my api key is wrong' do
-      user = User.find(@user.id)
+      user = User.find(@user2.id)
 
       expect(response).to_not be_successful
       expect(user.favorites).to eq([])
