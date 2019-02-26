558b5450490b2185bd7baaf911917dda30a73e93
Brad Niedt <bradleyniedt@gmail.com>
2019-02-26 15:14:46 -0700

    Final change to make all tests pass.

--- app/presenters/favorite_presenter.rb
+++ app/presenters/favorite_presenter.rb
@@ -1,16 +1,28 @@
 class FavoritePresenter
   attr_reader :id, :favorites
-  def initialize(favorites)
+  def initialize(favorites, method = 'make')
     @id = 1
     @faves = favorites
     @favorites = []
-    get_weather
+    if method == "delete"
+      delete_favorite
+    else
+      get_favorites
+    end
   end
 
-  def get_weather
+
+  def get_favorites
     @faves.each do |fave|
       weather = ForecastService.new.get_forecast(fave.location)
-      @favorites << { location: fave.location, current_weather: weather["currently"] }
+      @favorites << { "location" => fave.location, "current_weather" => weather["currently"] }
     end
+  end
+
+  def delete_favorite
+    weather = ForecastService.new.get_forecast(@faves.location)
+    @favorites << { "location" => @faves.location, "current_weather" => weather["currently"] }
+    Favorite.destroy(@faves.id)
+    @favorites
   end
 end
