8abe5df1e68aa2cb49079aa9ca14153ce9bb0085
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 19:24:00 -0700

    Adds user create favorite method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   validates_uniqueness_of :api_key
 
   has_secure_password
+
+  def create_favorite(location)
+    coords = GeocodeService.new.get_coords(location)
+    weather = Forecast.new.get_forecast("#{coords["lat"]},#{coords["lng"]}")
+    self.favorites.create!(location: location, current_weather: weather["currently"])
+    binding.pry
+  end
 end
