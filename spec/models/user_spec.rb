4dc22c098b8b628bc6aa868b8fdc521447a3f82f
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 19:17:32 -0700

    Adds user model test for create favorite.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,18 @@ describe User, type: :model do
     it { should validate_presence_of(:api_key)}
     it { should validate_uniqueness_of(:api_key)}
   end
+  describe 'instance methods' do
+    it 'create_favorite' do
+      key = SecureRandom.base64
+      user = User.create!(email: "bob@email.com", password: "pass", api_key: @key)
+      user.create_favorite("Denver, CO")
+      favorite = user.favorites.first
+
+      expect(favorite.user_id).to eq(user.id)
+      expect(favorite.location).to eq("Denver, CO")
+      expect(favorite.current_weather).to have_key("time")
+      expect(favorite.current_weather).to have_key("summary")
+      expect(favorite.current_weather).to have_key("temperature")
+    end
+  end
 end
