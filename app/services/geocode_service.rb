24aba8741b03ca96134cdbc4b3251696ed36dd20
Brad Niedt <bradleyniedt@gmail.com>
2019-02-26 13:31:46 -0700

    Adds caching to geocode service to store the location. Going to confirm all tests still work.

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -7,9 +7,16 @@ class GeocodeService
   end
 
   def get_coords(location)
-    response = conn.get("geocode/json?") do |request|
-      request.params["address"] = location
+    coord = Location.find_by(city: location)
+    if coord
+      return { "lat" => coord.latitude, "lng" => coord.longitude }
+    else
+      response = conn.get("geocode/json?") do |request|
+        request.params["address"] = location
+      end
+      data = JSON.parse(response.body)["results"].first["geometry"]["location"]
+      Location.create!(city: location, latitude: data["lat"], longitude: data["lng"])
+      data
     end
-    data = JSON.parse(response.body)["results"].first["geometry"]["location"]
   end
 end
