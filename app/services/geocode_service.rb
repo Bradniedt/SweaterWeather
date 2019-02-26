d10782e1b47df396efcdb4ceb7088ce6b0c6085b
Brad Niedt <bradleyniedt@gmail.com>
2019-02-26 13:47:20 -0700

    Adds caching refactor to geocode service.

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -6,17 +6,22 @@ class GeocodeService
     end
   end
 
+  def get_data(place)
+    response = conn.get("geocode/json?") do |request|
+      request.params["address"] = place
+    end
+    data = JSON.parse(response.body)["results"].first["geometry"]["location"]
+  end
+
   def get_coords(location)
     coord = Location.find_by(city: location)
     if coord
       return { "lat" => coord.latitude, "lng" => coord.longitude }
     else
-      response = conn.get("geocode/json?") do |request|
-        request.params["address"] = location
-      end
-      data = JSON.parse(response.body)["results"].first["geometry"]["location"]
+      data = get_data(location)
       Location.create!(city: location, latitude: data["lat"], longitude: data["lng"])
       data
     end
   end
+
 end
