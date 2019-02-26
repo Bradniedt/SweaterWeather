dfd4fefc7b802c6f72f9c6b036a3fce2a6a8cf38
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 20:09:36 -0700

    Removed pry.

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
-    binding.pry
     user = User.find_by(api_key: params["api_key"])
     if user
       user.favorites.create!(params["location"])
