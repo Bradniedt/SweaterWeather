3693d93c14ef9ecb479525f654059ac3738ae738
Brad Niedt <bradleyniedt@gmail.com>
2019-02-25 20:20:14 -0700

    Removed unnecessary expectations. Tests pass.

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -5,13 +5,12 @@ describe '(Favorites Endpoint) As a user' do
     before do
       @key1 = SecureRandom.base64
       @user1 = User.create!(email: "bob@email.com", password: "pass", api_key: @key1)
-      post '/api/v1/favorites', params: { "api_key" => "#{@key}", "location" => "Denver, CO" }
+      post '/api/v1/favorites', params: { "api_key" => "#{@key1}", "location" => "Denver, CO" }
     end
     it 'should add that location to my favorites' do
       user = User.find(@user1.id)
       favorite = user.favorites.first
-      
-      expect(response).to be_successful
+
       expect(favorite.user_id).to eq(user.id)
       expect(favorite.location).to eq("Denver, CO")
     end
